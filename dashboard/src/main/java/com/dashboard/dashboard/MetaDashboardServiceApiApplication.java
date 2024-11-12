2569df2e23d49bdea8f53278e9a9ec42da1bd3c6
paggybhangale <bhangaleparags@gmail.com>
2024-11-12 14:49:05 +0530

    added spring initializer base project

---
+++ dashboard/src/main/java/com/dashboard/dashboard/MetaDashboardServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaDashboardServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaDashboardServiceApiApplication.class, args);
+	}
+
+}
